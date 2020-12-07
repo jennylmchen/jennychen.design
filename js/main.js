a5878fd260fcf8028dbfc4f0ae3daf95ff9e8605
Ivon Liu <me@ivonliu.com>
2020-12-06 16:08:42 -0800

    State machine to track page and nav links

--- js/main.js
+++ js/main.js
@@ -4,31 +4,83 @@ var myShuffle = new Shuffle(document.querySelector('.my-shuffle'), {
 	buffer: 1
 });
 
+var state = Shuffle.ALL_ITEMS;
+var detached = false; // true if state is different from myShuffle.lastFilter
+
 $('#filter-all').click(function() {
+	detached = false;
+	state = Shuffle.ALL_ITEMS;
+	page('');
 	myShuffle.filter(Shuffle.ALL_ITEMS);
-	$('#filter-all').addClass('active');
-	$('#filter-architecture').removeClass('active');
-	$('#filter-illustrations').removeClass('active');
 });
 
 $('#filter-architecture').click(function() {
+	detached = false;
+	state = 'architecture';
+	page('');
 	myShuffle.filter('architecture');
-	$('#filter-all').removeClass('active');
-	$('#filter-architecture').addClass('active');
-	$('#filter-illustrations').removeClass('active');
 });
 
 $('#filter-illustrations').click(function() {
+	detached = false;
+	state = 'illustrations';
+	page('');
 	myShuffle.filter('illustrations');
-	$('#filter-all').removeClass('active');
-	$('#filter-architecture').removeClass('active');
-	$('#filter-illustrations').addClass('active');
 });
 
-function projects(name) {
-	newUrl = 'projects/' + name + '/';
-	if (window.location.protocol == 'file:') {
-		newUrl += 'index.html';
+function page(name) {
+	var innerPage = ''
+	if (name == 'about') {
+		innerPage = 'about/index.html';
+		detached = true;
+		state = 'about';
+		loadInnerPage(innerPage);
+		setActiveLink();
+	} else if (name == 'contact') {
+		innerPage = 'contact/index.html';
+		detached = true;
+		state = 'contact';
+		loadInnerPage(innerPage);
+		setActiveLink();
+	} else if (name) {
+		innerPage = 'projects/' + name + '/index.html';
+		loadInnerPage(innerPage);
+		$('.my-link').removeClass('active');
+	} else {
+		if (detached) {
+			detached = false;
+			state = myShuffle.lastFilter;
+		}
+		$('.detail-container').hide();
+		$('.my-shuffle').show();
+		$('.detail-container').html('');
+		setActiveLink();
 	}
-	location.href = newUrl;
+}
+
+function loadInnerPage(innerPage) {
+	$('.detail-container').show();
+	$('.my-shuffle').hide();
+	$('.detail-container').load(innerPage);
+}
+
+function setActiveLink() {
+	$('.my-link').removeClass('active');
+	switch(state) {
+		case Shuffle.ALL_ITEMS:
+			$('#filter-all').addClass('active');
+			break;
+		case 'architecture':
+			$('#filter-architecture').addClass('active');
+			break;
+		case 'illustrations':
+			$('#filter-illustrations').addClass('active');
+			break;
+		case 'about':
+			$('#about-page').addClass('active');
+			break;
+		case 'contact':
+			$('#contact-page').addClass('active');
+			break;
+	}
 }
