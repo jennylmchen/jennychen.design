225277914f9c7002f17a08d80671a213d72c45e4
Ivon Liu <me@ivonliu.com>
2020-12-06 20:59:47 -0800

    Add support for individual project pages (#2)
    * Fix top spacing on mobile
    
    * State machine to track page and nav links
    
    * Use markdown for pages
    
    * Nav between posts
    
    * Dynamically build shuffle figures from JSON
    
    * Working page navigation buttons
    
    * Page nav arrow styling
    
    * Rename

--- js/main.js
+++ js/main.js
@@ -1,34 +1,166 @@
 var myShuffle = new Shuffle(document.querySelector('.my-shuffle'), {
-	itemSelector: '.image-item',
-	sizer: '.my-sizer-element',
-	buffer: 1
+    itemSelector: '.image-item',
+    sizer: '.my-sizer-element',
+    buffer: 1
 });
 
+var state = Shuffle.ALL_ITEMS;
+var detached = false; // true if state is different from myShuffle.lastFilter
+
+var displayedPages = [];
+var pageIndices = {};
+
+$(document).ready(function() {
+    buildShuffleList();
+    readShuffleList();
+});
+
+function buildShuffleList() {
+    var figures = [];
+    var shuffleList = $('.my-shuffle')[0];
+    var sizer = $('.my-sizer-element')[0];
+
+    PAGES.forEach(function(page, idx) {
+        var figure = document.createElement('figure');
+        figure.classList.add('image-item', 'col-md-4');
+        figure.setAttribute('data-groups', `["${page.group}"]`);
+        figure.innerHTML = `
+            <div class="aspect">
+                <div class="aspect__inner">
+                    <img class="my-image" src="images/${page.name}/${page.image}"/>
+                    <div class="image-overlay" onclick="showPage('${page.name}')">
+                        <p>${page.description}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+        shuffleList.insertBefore(figure, sizer);
+        figures.push(figure);
+    });
+
+    myShuffle.add(figures);
+}
+
 $('#filter-all').click(function() {
-	myShuffle.filter(Shuffle.ALL_ITEMS);
-	$('#filter-all').addClass('active');
-	$('#filter-architecture').removeClass('active');
-	$('#filter-illustrations').removeClass('active');
+    detached = false;
+    state = Shuffle.ALL_ITEMS;
+    showPage('');
+    myShuffle.filter(Shuffle.ALL_ITEMS);
+    readShuffleList();
 });
 
 $('#filter-architecture').click(function() {
-	myShuffle.filter('architecture');
-	$('#filter-all').removeClass('active');
-	$('#filter-architecture').addClass('active');
-	$('#filter-illustrations').removeClass('active');
+    detached = false;
+    state = 'architecture';
+    showPage('');
+    myShuffle.filter('architecture');
+    readShuffleList();
 });
 
 $('#filter-illustrations').click(function() {
-	myShuffle.filter('illustrations');
-	$('#filter-all').removeClass('active');
-	$('#filter-architecture').removeClass('active');
-	$('#filter-illustrations').addClass('active');
+    detached = false;
+    state = 'illustrations';
+    showPage('');
+    myShuffle.filter('illustrations');
+    readShuffleList();
 });
 
-function projects(name) {
-	newUrl = 'projects/' + name + '/';
-	if (window.location.protocol == 'file:') {
-		newUrl += 'index.html';
-	}
-	location.href = newUrl;
+function showPage(name, showPageNav=true) {
+    if (name == 'about') {
+        var innerPage = 'about.md';
+        detached = true;
+        state = 'about';
+        loadInnerPage(innerPage);
+        setActiveLink();
+    } else if (name == 'contact') {
+        var innerPage = 'contact.md';
+        detached = true;
+        state = 'contact';
+        loadInnerPage(innerPage);
+        setActiveLink();
+    } else if (name) {
+        var innerPage = `pages/${name}.md`;
+        loadInnerPage(innerPage);
+        $('.my-link').removeClass('active');
+    } else {
+        if (detached) {
+            detached = false;
+            state = myShuffle.lastFilter;
+        }
+        $('.detail-container').hide();
+        $('.my-shuffle').show();
+        $('.md-container').html('');
+        setActiveLink();
+        return;
+    }
+
+    if (!showPageNav) {
+        $('.page-nav').hide();
+    } else {
+        $('.page-nav').show();
+        setPageNav(name);
+    }
+}
+
+function loadInnerPage(innerPage) {
+    $('.md-container').html(
+        `<zero-md src="${innerPage}"></zero-md>`
+    );
+    $('.detail-container').show();
+    $('.my-shuffle').hide();
+}
+
+function readShuffleList() {
+    displayedPages = PAGES.filter(function(page) {
+        return myShuffle.lastFilter == Shuffle.ALL_ITEMS || page.group == myShuffle.lastFilter;
+    });
+
+    pageIndices = {};
+    displayedPages.forEach(function(page, idx) {
+        pageIndices[page.name] = idx;
+    });
+}
+
+function setPageNav(name) {
+    var idx = pageIndices[name];
+    var n = displayedPages.length;
+
+    if (idx > 0) {
+        var prevPage = displayedPages[idx - 1];
+        $('#prev-page').html(`&#8592; ${prevPage.title}`);
+        $('#prev-page').attr('onclick', `showPage("${prevPage.name}")`);
+        $('#prev-page').css('visibility', 'visible');
+    } else {
+        $('#prev-page').css('visibility', 'hidden');
+    }
+
+    if (idx < n - 1) {
+        var nextPage = displayedPages[idx + 1];
+        $('#next-page').html(`${nextPage.title} &#8594;`);
+        $('#next-page').attr('onclick', `showPage("${nextPage.name}")`);
+        $('#next-page').css('visibility', 'visible');
+    } else {
+        $('#next-page').css('visibility', 'hidden');
+    }
+}
+
+function setActiveLink() {
+    $('.my-link').removeClass('active');
+    switch(state) {
+        case Shuffle.ALL_ITEMS:
+            $('#filter-all').addClass('active');
+            break;
+        case 'architecture':
+            $('#filter-architecture').addClass('active');
+            break;
+        case 'illustrations':
+            $('#filter-illustrations').addClass('active');
+            break;
+        case 'about':
+            $('#about-page').addClass('active');
+            break;
+        case 'contact':
+            $('#contact-page').addClass('active');
+            break;
+    }
 }
---
+++ js/pages.js
@@ -0,0 +1,103 @@
+const PAGES = [{
+	"name": "100a",
+	"image": "cover_small.png",
+	"group": "architecture",
+	"title": "Architecture 100a",
+	"description": "hello"
+}, {
+	"name": "11a",
+	"image": "altcover_small.jpg",
+	"group": "architecture",
+	"title": "Architecture 11a",
+	"description": "hello"
+}, {
+	"name": "11b",
+	"image": "axon_small.jpg",
+	"group": "architecture",
+	"title": "Architecture 11b",
+	"description": "hello"
+}, {
+	"name": "delsol",
+	"image": "cover_small.jpg",
+	"group": "architecture",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "brb",
+	"image": "cover_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "dimsum",
+	"image": "HarGao_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "election",
+	"image": "cover_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "gryffin",
+	"image": "cover_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "pameranposkad",
+	"image": "cover_small.jpg",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "rocknroller",
+	"image": "jennychen_rocknrollerlogo1_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "ssashirt",
+	"image": "ssashirt_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "urbansketching",
+	"image": "cover_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "warp",
+	"image": "cover_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "summer",
+	"image": "jenny_personalproject_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "turkey",
+	"image": "turkey1_small.jpeg",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "celebration",
+	"image": "cover_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}, {
+	"name": "change",
+	"image": "cover_small.png",
+	"group": "illustrations",
+	"title": "Title",
+	"description": "hello"
+}];
