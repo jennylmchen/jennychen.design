175627f314a405d7371ca4b751d797d7696859f4
Jenny Chen <46910199+jennylmchen@users.noreply.github.com>
2020-12-12 23:12:25 -0800

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -2,102 +2,102 @@ const PAGES = [{
 	"name": "100a",
 	"image": "cover_small.png",
 	"group": "architecture",
-	"title": "Architecture 100a",
-	"description": "hello"
+	"title": "CCCC",
+	"description": "Contra Costa Community Commons (2020)"
 }, {
 	"name": "11a",
 	"image": "altcover_small.jpg",
 	"group": "architecture",
-	"title": "Architecture 11a",
-	"description": "hello"
+	"title": "Projection",
+	"description": "Projection (2019)"
 }, {
 	"name": "11b",
 	"image": "axon_small.jpg",
 	"group": "architecture",
-	"title": "Architecture 11b",
-	"description": "hello"
+	"title": "Duplex",
+	"description": "Duplex (2020)"
 }, {
 	"name": "delsol",
 	"image": "cover_small.jpg",
 	"group": "architecture",
-	"title": "Title",
-	"description": "hello"
+	"title": "Del Sol Plaza",
+	"description": "Urban Design Study - Del Sol Plaza (2020)"
 }, {
 	"name": "brb",
 	"image": "cover_small.png",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "BRB",
+	"description": "Article Illustrations (2019-20)"
 }, {
 	"name": "dimsum",
 	"image": "HarGao_small.png",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "Dim Sum",
+	"description": "Dim Sum Patches (2019)"
 }, {
 	"name": "election",
 	"image": "cover_small.png",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "Shape of Politics",
+	"description": "Shape of Politics (2020)"
 }, {
 	"name": "gryffin",
 	"image": "cover_small.png",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "Gryffin Shirt Design",
+	"description": "Ignite the Passion (2017)"
 }, {
 	"name": "pameranposkad",
 	"image": "cover_small.jpg",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "Row Houses Across Space",
+	"description": "Row Houses Across Space (2020)"
 }, {
 	"name": "rocknroller",
 	"image": "jennychen_rocknrollerlogo1_small.png",
 	"group": "illustrations",
 	"title": "Title",
-	"description": "hello"
+	"description": "Rock N Roller Logo (2020)"
 }, {
 	"name": "ssashirt",
 	"image": "ssashirt_small.png",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "SSA Shirt Design",
+	"description": "SSA Shirt (2019)"
 }, {
 	"name": "urbansketching",
 	"image": "cover_small.png",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "Urban Sketches",
+	"description": "Urban Sketches (2017-20)"
 }, {
 	"name": "warp",
 	"image": "cover_small.png",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "Game Design",
+	"description": "Game Design (2019)"
 }, {
 	"name": "summer",
 	"image": "jenny_personalproject_small.png",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "Summer",
+	"description": "Summer (2020)"
 }, {
 	"name": "turkey",
 	"image": "turkey1_small.jpeg",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "Turkey",
+	"description": "Turkey (2017)"
 }, {
 	"name": "celebration",
 	"image": "cover_small.png",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "Celebration",
+	"description": "Celebration (2020)"
 }, {
 	"name": "change",
 	"image": "cover_small.png",
 	"group": "illustrations",
-	"title": "Title",
-	"description": "hello"
+	"title": "Change",
+	"description": "Change (2020)"
 }];
