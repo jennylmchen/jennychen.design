0a34aae6b06eaaac41ba08c4d6f4b5622a1606b9
Jenny Chen <jennylmchen@berkeley.edu>
2024-06-19 16:53:29 -0400

    added more covers

--- js/pages.js
+++ js/pages.js
@@ -5,11 +5,11 @@ const PAGES = [{
 	"title": "Mile Marker",
 	"description": "Mile Marker: An Architect's Catalog of Designs for the Last Mile of Parcel Delivery (2024)"
 }, {
-	"name": "objects",
-	"image": "deform.jpg",
+	"name": "guatemala",
+	"image": "cover.png",
 	"group": "architecture",
-	"title": "Waste Too, Want Too",
-	"description": "Waste Too, Want Too (2023)"
+	"title": "Pla2a",
+	"description": "Pla2a: Activation of Public Space Through Soft Enclosure (2023)"
 }, {
 	"name": "vc",
 	"image": "cover.jpg",
@@ -17,23 +17,101 @@ const PAGES = [{
 	"title": "Syracuse",
 	"description": "A Little Bit of Syracuse (2023)"
 }, {
+	"name": "ai",
+	"image": "cover.png",
+	"group": "architecture",
+	"title": "Elements of an (AI) Architecture",
+	"description": "Elements of an (AI) Architecture (2024)"
+}, {
+	"name": "dozen",
+	"image": "DOZEN.jpg",
+	"group": "branding",
+	"title": "DOZEN",
+	"description": "DOZEN (2023)"
+}, {
+	"name": "objects",
+	"image": "deform.jpg",
+	"group": "architecture",
+	"title": "Waste Too, Want Too",
+	"description": "Waste Too, Want Too (2023)"
+}, {
+	"name": "rural",
+	"image": "cover.jpg",
+	"group": "illustrations",
+	"title": "Rural Intensification",
+	"description": "Rural Intensification (2024)"
+}, {
+	"name": "dubois",
+	"image": "cover.jpg",
+	"group": "illustrations",
+	"title": "Modern Du Bois",
+	"description": "Modern Du Bois: Technologically Enhanced Discrimination (2024)"
+}, {
+	"name": "jkpostcards",
+	"image": "samik_front.jpg",
+	"group": "illustrations",
+	"title": "Hello From...",
+	"description": "Hello From...: Residential Community Spaces in Japan and Korea (2023)"
+}, {
+	"name": "seoul",
+	"image": "cover.jpg",
+	"group": "architecture",
+	"title": "Seoul Spider",
+	"description": "BIG Project: Seoul Spider Yeouido (2023)"
+}, {
 	"name": "busan",
 	"image": "typologies.png",
 	"group": "architecture",
 	"title": "Nature's Edge",
 	"description": "The Urban Designer's Guide to the Nature's Edge (2023)"
 },	{
+	"name": "tokyo",
+	"image": "cover.png",
+	"group": "architecture",
+	"title": "Digital Flaneurs",
+	"description": "Digital Flaneurs of Harajuku (2023)"
+}, {
+	"name": "606",
+	"image": "cover.png",
+	"group": "architecture",
+	"title": "Design Futuring",
+	"description": "Design Futuring (2022)"
+}, {
+	"name": "lowrise",
+	"image": "ext1.png",
+	"group": "architecture",
+	"title": "Low Rise LA",
+	"description": "Low Rise LA Competition (2022)"
+}, {
+	"name": "100d",
+	"image": "model.jpg",
+	"group": "architecture",
+	"title": "Designing Domesticity",
+	"description": "Designing Domesticity (2022)"
+}, {
+	"name": "100c",
+	"image": "s1edited.png",
+	"group": "architecture",
+	"title": "Wildfires",
+	"description": "Wildfires (2021)"
+}, {
+	"name": "100b",
+	"image": "rainy_day.png",
+	"group": "architecture",
+	"title": "Urban Institute",
+	"description": "The Urban Institute (2021)"
+}, {
+	"name": "masque",
+	"image": "2img.png",
+	"group": "illustrations",
+	"title": "Pandemic Masque",
+	"description": "Pandemic Masque (2020)"
+}, {
 	"name": "100a",
 	"image": "cover_small.png",
 	"group": "architecture",
 	"title": "CCCC",
 	"description": "Contra Costa Community Commons (2020)"
-}, {
-	"name": "11a",
-	"image": "altcover_small.jpg",
-	"group": "architecture",
-	"title": "Projection",
-	"description": "Projection (2019)"
 }, {
 	"name": "11b",
 	"image": "axon_small.jpg",
@@ -52,6 +130,12 @@ const PAGES = [{
 	"group": "architecture",
 	"title": "Del Sol Plaza",
 	"description": "Urban Design Study (2020)"
+}, {
+	"name": "11a",
+	"image": "altcover_small.jpg",
+	"group": "architecture",
+	"title": "Projection",
+	"description": "Projection (2019)"
 }, {
 	"name": "planemo",
 	"image": "planemo.png",
